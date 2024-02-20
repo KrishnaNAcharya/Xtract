8f3e009af0f6233f22cfc39ea9c92b8981f26879
Avyay <112500711+AvyayNayak@users.noreply.github.com>
2024-02-21 00:47:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const { spawn } = require("child_process");
 
 //------------------setting up app and port--------------------------------------
 const port = 5173;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  next();
+});
 
 const app = express();
 app.use(express.static(__dirname));
